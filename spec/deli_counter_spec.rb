0936b6f606d1c4d7bbe17d59b07e4d5fc6eaf598
Tarric Sookdeo <tarricsookdeo@outlook.com>
2019-10-10 21:46:33 +0000

    Done.

--- spec/deli_counter_spec.rb
+++ spec/deli_counter_spec.rb
@@ -20,7 +20,7 @@ describe 'Deli Counter' do
         line(other_deli)
       end
 
-      it 'works with different people in line' do
+      it 'works with different people in line' do`
 	      expect($stdout).to receive(:puts).with("The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey")
 	line(another_deli)
       end
