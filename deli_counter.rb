e8daf71bb48183d5c42c88edaf39e7015f819823
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-12 01:21:19 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+katz_deli = []
+def line (katz_deli)
+  customer_line= []
+if katz_deli.empty?
+  puts "The line is currently empty."
+else
+   line_num = 1
+   katz_deli.each do |customer|
+     customer_line << "#{line_num}. #{customer}"
+     line_num += 1
+   end
+   puts "The line is currently: #{customer_line.join(" ")}"
+ end
+end
+end 
+end
+
+
+
