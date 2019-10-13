9d226f3bbe4a7682d3457f408e06374e79f7e7ec
William Pannell <lpann5@gmail.com>
2019-10-13 23:50:22 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(array)
+  x=0
+  if array.size==0
+    return "The line is currently empty."
+  elsif array.size > 0
+    return "The line is currently: #{array[x]}"
+  end
+end
+  
+
+def take_a_number(array, string)
+  katz_deli = []
+  
+  
+end
