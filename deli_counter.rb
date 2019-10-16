0b93794c98314c45ce41415aca6e304cda0524fc
William Pannell <lpann5@gmail.com>
2019-10-16 03:04:29 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,12 @@
 def line(array)
   x=0
 if array.size != 0
-  puts "The line is currently:"
+  message = "The line is currently:"
 while x != array.size
-    puts " #{x}. #{array[x]}"
-    x=+1
+    message << " #{x+1}. #{array[x]}"
+    x+=1
 end
+  puts message
 end
 if array.size == 0
   puts "The line is currently empty."
@@ -14,7 +15,16 @@ end
 end
 
 def take_a_number(array, string)
-  katz_deli = []
-  
-  
+  x=array.size+1
+  array[array.size]=string
+  puts "Welcome, #{string}. You are number #{x} in line."
+end
+
+def now_serving(array)
+  thenext = array.shift
+  if array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+  puts "Currently serving #{thenext}."
+  end
 end
