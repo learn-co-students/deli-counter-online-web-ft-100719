d241f13e19843a8e0042bc918844f70ad1933b49
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-10-21 01:45:08 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,3 +10,19 @@ def line(katz_deli)
     puts current_line
   end
 end
+
+def take_a_number (katz_deli,name)
+  if katz_deli.length==1
+    puts "Welcome, #{name}. You are number #{counter+1} in line."
+  end
+  katz_deli
+end
+
+#describe "#take_a_number" do
+    #context "there is nobody in line" do
+      #it "should add a person to the line" do
+       # expect($stdout).to receive(:puts).with("Welcome, Ada. You are number 1 in line.")
+        #take_a_number(katz_deli, "Ada")
+        #expect(katz_deli).to eq(["Ada"])
+      #end
+    #end
