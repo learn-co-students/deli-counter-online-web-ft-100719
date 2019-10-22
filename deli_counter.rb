70ef5f48bc8f62b05d0111d5a2083a3e14646498
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-10-22 21:54:23 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,18 @@ def take_a_number (katz_deli,name)
       puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   katz_deli
 end
+
+def now_serving(katz_deli)
+  if katz_deli.length==0
+  puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+  end
+  katz_deli.shift
+end
+
+#context "there are people in line" do
+      #it "should serve the first person in line and remove them from the queue" do
+        #expect($stdout).to receive(:puts).with("Currently serving Logan.")
+        #now_serving(other_deli)
+        #expect(other_deli).to eq(%w(Avi Spencer))
